d325b95b3bb9c5c93b874b877cdf053e32736638
lwch <lwch748@gmail.com>
2022-06-08 16:09:59 +0800

    修正api接口埋点名称问题

--- code/app/api.go
+++ code/app/api.go
@@ -17,7 +17,8 @@ func (app *App) reg(uri string, cb func(*client.Clients, *api.Context)) {
 			http.Error(w, "raise limit", http.StatusServiceUnavailable)
 			return
 		}
-		statName := strings.ReplaceAll(uri, "/", ":")
+		statName := strings.TrimSuffix(uri, "/")
+		statName = strings.ReplaceAll(statName, "/", ":")
 		counter := app.stats.NewCounter("api_counter" + statName)
 		counter.Inc()
 		tick := app.stats.NewTick("api_pref" + statName)
